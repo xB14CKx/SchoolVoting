0aa0590459dc2c743e0842cba38e5206920043b5
xB14CKx <xBLACKJONESx@gmail.com>
2025-03-30 17:58:51 +0800

    Adding/Editing Files

--- resources/views/partials/topbar.blade.php
+++ resources/views/partials/topbar.blade.php
@@ -1,61 +1,18 @@
-<!-- resources/views/partials/topbar.blade.php -->
-<style>
-    /* Navigation Bar Styles */
-    .navbar {
-      width: 100%;
-      height: 55px;
-      display: flex;
-      justify-content: flex-end;
-      align-items: center;
-      background-color: rgba(0, 0, 0, 70);
-    }
-    
-    .nav-links {
-      display: flex;
-      gap: 95px;
-      margin-right: 36px;
-      list-style: none;
-      padding: 0;
-    }
-    
-    .nav-item a {
-      color: #fff;
-      font-family: "Istok Web", sans-serif;
-      font-size: 15px;
-      font-weight: 400;
-      cursor: pointer;
-      text-decoration: none;
-    }
-    
-    /* Responsive Styles */
-    @media (max-width: 991px) {
-      .nav-links {
-        gap: 40px;
-        margin-right: 20px;
-      }q
-    }
-    
-    @media (max-width: 640px) {
-      .nav-links {
-        display: none;
-      }
-    }
-    </style>
-    
-    <nav class="navbar">
-      <ul class="nav-links">
-        <li class="nav-item">
-          <a href="#about"><i class="fa-solid fa-question"></i> &nbsp; About</a>
-        </li>
-        <li class="nav-item">
-          <a href="#contact"><i class="fa-solid fa-address-book"></i> &nbsp; Contact</a>
-        </li>
-        <li class="nav-item">
-          <a href="{{ route('login') }}">
-            <i class="fa-solid fa-right-to-bracket"></i> &nbsp; 
-            <span style="color: #FFdF61;">Login</span>
-          </a>
-        </li>
-      </ul>
-    </nav>
-    
+@vite(['resources/css/topbar.css'])
+
+<nav class="navbar">
+  <ul class="nav-links">
+    <li class="nav-item">
+      <a href="{{ route('about')}}"><i class="fa-solid fa-question"></i> &nbsp; About</a>
+    </li>
+    <li class="nav-item">
+      <a href="#contact"><i class="fa-solid fa-address-book"></i> &nbsp; Contact</a>
+    </li>
+    <li class="nav-item">
+      <a href="{{ route('login') }}">
+        <i class="fa-solid fa-right-to-bracket"></i> &nbsp;
+        <span style="color: #FFdF61;">Login</span>
+      </a>
+    </li>
+  </ul>
+</nav>
