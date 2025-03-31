716b6dd77b83a2b0f073b72473cfd0bb28ca6b85
xB14CKx <xBLACKJONESx@gmail.com>
2025-04-01 01:57:53 +0800

    Adding/Editing files

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
+                'resources/css/app.css',
                 'resources/css/Landing.css',
                 'resources/css/eligibility.css',
                 'resources/css/about.css',
